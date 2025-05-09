cb7ef200223e6be8d146e3403f4b5b4dc52d19f5
Brian Tomadin <tomadinbrian@gmail.com>
2025-05-09 10:14:43 -0300

    Corrección ubicación html

---
+++ src/main/java/Fundamentos/de/Analisis/de/Sistemas/controladores/ConcesionariaController.java
@@ -0,0 +1,18 @@
+
+package Fundamentos.de.Analisis.de.Sistemas.controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class ConcesionariaController {
+    
+    @GetMapping("/")
+    public String concesionaria(){
+        return "VistaPrincipal.html";
+    }
+    
+    
+}
