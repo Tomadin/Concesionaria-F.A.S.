06fa2b88398f3f9a9af2aea2b5b0ee3a9bf3c279
JoaquinYornet <joaquinyornetderosas@gmail.com>
2025-05-12 02:49:06 -0300

    Funcion para ordenar vista stock, formularios (no funcionales aun) añadidos,
    Añadí una funcíon que permite ordenar de manera ascendente o descendente los elemenos en la vista de stock de acuerdo a los atributos mostrados en la tabla.
    Añadñi los formularios restantes, aún no reaccionan

--- src/main/resources/static/js/script_formularios.js
+++ src/main/resources/static/js/script_formularios.js
@@ -2,7 +2,7 @@ function inicio_pagina_formulario(){
 
 }
 
-
+//Para el formulario de vehiculo - Formulario 1 - F1
 document.getElementById("formulario_vehiculo").addEventListener('submit', function(e) {
     e.preventDefault();
 
@@ -10,97 +10,96 @@ document.getElementById("formulario_vehiculo").addEventListener('submit', functi
     let operacion_exitosa = true;
     const mensajes_de_error = {};
 
-    const f_modelo = document.getElementById("modelo");
+    const f_modelo = document.getElementById("F1_modelo");
     if(f_modelo){
-        objeto["modelo"] = f_modelo.value;
+        objeto["F1_modelo"] = f_modelo.value;
     }else{
         operacion_exitosa = false;
-        mensajes_de_error["modelo"] = "Debe especificar el modelo";
+        mensajes_de_error["F1_modelo"] = "Debe especificar el modelo";
     }
 
-    const f_version = document.getElementById("version");
+    const f_version = document.getElementById("F1_version");
     if(f_version){
-        objeto["version"] = f_version.value;
+        objeto["F1_version"] = f_version.value;
     }else{
         operacion_exitosa = false;
-        mensajes_de_error["version"] = "Debe especificar la versión";
+        mensajes_de_error["F1_version"] = "Debe especificar la versión";
     }
 
-    const f_proveedor = document.getElementById("proveedor");
+    const f_proveedor = document.getElementById("F1_proveedor");
     if(f_proveedor){
-        objeto["proveedor"] = f_proveedor.value;
+        objeto["F1_proveedor"] = f_proveedor.value;
     }else{
         operacion_exitosa = false;
-        mensajes_de_error["proveedor"] = "El proveedor es requerido";
+        mensajes_de_error["F1_proveedor"] = "El proveedor es requerido";
     }
 
-    const f_matricula = document.getElementById("matricula");
+    const f_matricula = document.getElementById("F1_matricula");
     if(f_matricula){
-        objeto["matricula"] = f_matricula.value;
+        objeto["F1_matricula"] = f_matricula.value;
     }
 
-    const f_precio = document.getElementById("precio");
+    const f_precio = document.getElementById("F1_precio");
     if(f_precio){
-        objeto["precio"] = f_precio.value;
+        objeto["F1_precio"] = f_precio.value;
     }else{
         operacion_exitosa = false;
-        mensajes_de_error["precio"] = "El precio es requerido";
+        mensajes_de_error["F1_precio"] = "El precio es requerido";
     }
 
-    const f_color = document.getElementById("color");
+    const f_color = document.getElementById("F1_color");
     if(f_color){
-        objeto["color"] = f_color.value;
+        objeto["F1_color"] = f_color.value;
     }else{
         operacion_exitosa = false;
-        mensajes_de_error["color"] = "El color es requerido";
+        mensajes_de_error["F1_color"] = "El color es requerido";
     }
 
-    const f_anio = document.getElementById("anio");
+    const f_anio = document.getElementById("F1_anio");
     if(f_anio){
-        objeto["anio"] = f_anio.value;
+        objeto["F1_anio"] = f_anio.value;
     }else{
         operacion_exitosa = false;
-        mensajes_de_error["anio"] = "El año es requerido";
+        mensajes_de_error["F1_anio"] = "El año es requerido";
     }
 
-    const f_serie = document.getElementById("serie");
+    const f_serie = document.getElementById("F1_serie");
     if(f_serie){
-        objeto["serie"] = f_serie.value;
+        objeto["F1_serie"] = f_serie.value;
     }else{
         operacion_exitosa = false;
-        mensajes_de_error["serie"] = "El nro de serie es requerido";
+        mensajes_de_error["F1_serie"] = "El nro de serie es requerido";
     }
 
-    const f_kilometraje = document.getElementById("kilometraje");
+    const f_kilometraje = document.getElementById("F1_kilometraje");
     if(f_kilometraje){
-        objeto["kilometraje"] = f_kilometraje.value;
+        objeto["F1_kilometraje"] = f_kilometraje.value;
     }else{
         operacion_exitosa = false;
-        mensajes_de_error["kilometraje"] = "El kilometraje es requerido";
+        mensajes_de_error["F1_kilometraje"] = "El kilometraje es requerido";
     }
 
-    const f_estado = document.getElementById("estado");
+    const f_estado = document.getElementById("F1_estado");
     if(f_estado){
-        objeto["estado"] = f_estado.value;
+        objeto["F1_estado"] = f_estado.value;
     }else{
         operacion_exitosa = false;
-        mensajes_de_error["estado"] = "Debe indicar el estado del vehículo";
+        mensajes_de_error["F1_estado"] = "Debe indicar el estado del vehículo";
     }
 
-    var extensionesValidas = 
-            /(\.jpg|\.jpeg|\.png)$/i;
+    var extensionesValidas = /(\.jpg|\.jpeg|\.png)$/i;
 
-    const f_imagen = document.getElementById("imagen");
+    const f_imagen = document.getElementById("F1_imagen");
     if(f_imagen){
         if(!extensionesValidas.exec(f_imagen.value)){
             operacion_exitosa = false;
-            mensajes_de_error["imagen"] = "El formato de la imagen debe ser .jpg, .jpeg o .png";
+            mensajes_de_error["F1_imagen"] = "El formato de la imagen debe ser .jpg, .jpeg o .png";
         }else{
-            objeto["imagen"] = f_imagen.value;
+            objeto["F1_imagen"] = f_imagen.value;
         }
     }else{
         operacion_exitosa = false;
-        mensajes_de_error["imagen"] = "Debe subir una imágen";
+        mensajes_de_error["F1_imagen"] = "Debe subir una imágen";
     }
 
     if(operacion_exitosa){
--- src/main/resources/static/js/script_vista_principal.js
+++ src/main/resources/static/js/script_vista_principal.js
@@ -1,30 +1,27 @@
 function inicio(){ //funcion llamada por el body del html cada vez que se carga la página
 
-    for(let i=0; i<10; i++){
-        nueva_fila();
-    }
+    array_filas = [];
+
+    //debug, filas de ejemplo
+    nueva_fila(1, "Logan", "Life 1.6", "Gris", 25000000, "1234", 2019, "proveedor1", "0Km", 5);
+    nueva_fila(2, "Kangoo", "Zen 1.6", "Rojo", 30000000, "1634", 2020, "proveedor2", "0Km", 2);
+    nueva_fila(3, "Sandero", "Life 1.6", "Azul", 22000000, "2345", 2021, "proveedor3", "Usado", 3);
+    nueva_fila(4, "Kardian", "Evolution", "Blanco", 28000000, "3456", 2022, "proveedor4", "0Km", 4);
+    nueva_fila(5, "Clio", "Mio 1.2", "Negro", 11000000, "4567", 1994, "proveedor5", "Usado", 1);
+
+    ordenar_tabla();
+    
 }
 
+var array_filas = []; //array que contiene las filas de la tabla
 
-var asignador_de_id_filas = 0;
-/*cada vez que una fila es agregada a la tabla de stock, se le asigna un id usando la variable global de arriba.
-Luego la variable global aumenta para que la próxima fila tenga un id distinto
-*/
-function asignar_id_de_fila(){
-    asignador_de_id_filas++;
-    return (asignador_de_id_filas - 1); 
-}
-
-
-function nueva_fila(){
-
-    const nuevo_id = asignar_id_de_fila()
+function nueva_fila(vehiculo_id, modelo, version, color, precio, serie, anio, proveedor, estado, stock){
 
     const tabla = document.getElementById("tabla_stock");
 
     const fila = document.createElement("tr"); //filas con los datos: modelo, precio, stock
+    fila.id = "vehiculo_" + vehiculo_id;
     fila.className = "Fila_stock";
-
 
     const celda_imagen = document.createElement("td");
     const imagen = document.createElement("img");
@@ -32,37 +29,39 @@ function nueva_fila(){
     const celda_id_vehiculo = document.createElement("td");
     const celda_modelo = document.createElement("td");
     const celda_version = document.createElement("td");
+    const celda_color = document.createElement("td");
     const celda_precio = document.createElement("td");
     const celda_nro_serie = document.createElement("td");
     const celda_año = document.createElement("td")
-    const celda_color = document.createElement("td");
     const celda_proveedor = document.createElement("td");
+    const celda_estado = document.createElement("td");
     const celda_stock = document.createElement("td");
 
-
-    imagen.src = "images/renault-foto-placeholder.jpg";
+    imagen.src = "images/imagen-placeholder-para-autos.jpg";
     imagen.className = "Imagenes_auto_filas";
     celda_imagen.appendChild(imagen);
 
-    celda_id_vehiculo.innerHTML = "auto_" + nuevo_id;
-    celda_modelo.innerHTML = "modelo_placeholder";
-    celda_version.innerHTML = "version_placeholder";
-    celda_precio.innerHTML = "$0";
-    celda_nro_serie.innerHTML = "nro_serie_placeholder";
-    celda_año.innerHTML = "2025";
-    celda_color.innerHTML = "color_placeholder";
-    celda_proveedor.innerHTML = "proveedor_placeholder";
-    celda_stock.innerHTML = "Disponibilidad";
+    celda_id_vehiculo.innerHTML = vehiculo_id;
+    celda_modelo.innerHTML = modelo;
+    celda_version.innerHTML = version;
+    celda_color.innerHTML = color;
+    celda_precio.innerHTML = "$" + precio;
+    celda_nro_serie.innerHTML = serie;
+    celda_año.innerHTML = anio;
+    celda_proveedor.innerHTML = proveedor;
+    celda_estado.innerHTML = estado;
+    celda_stock.innerHTML = stock;
 
     fila.appendChild(celda_imagen);
     fila.appendChild(celda_id_vehiculo);
     fila.appendChild(celda_modelo);
     fila.appendChild(celda_version);
+    fila.appendChild(celda_color);
     fila.appendChild(celda_precio);
     fila.appendChild(celda_nro_serie);
     fila.appendChild(celda_año);
-    fila.appendChild(celda_color);
     fila.appendChild(celda_proveedor);
+    fila.appendChild(celda_estado);
     fila.appendChild(celda_stock);
 
     // pasa el ID de la fila como parámetro al hacer click en la fila
@@ -70,13 +69,96 @@ function nueva_fila(){
         abrir_vista_auto(this.id); 
     });
 
-    fila.id = "id_stock_" + nuevo_id;
-
     tabla.appendChild(fila);
+    //ordenar_tabla();
+
+    array_filas.push(fila);
+    
 
 }
 
+function ordenar_tabla(){
 
-function abrir_vista_auto(id){
-    alert(id);
+    const tabla = document.getElementById("tabla_stock");
+
+    const hijos = Array.from(tabla.children);
+    hijos.forEach(hijo => {
+      if (hijo.tagName !== "TBODY") { //borramos todas las filas menos la cabecera
+        hijo.remove();
+      }
+    });
+
+    const variable_orden = document.getElementById("select_ordenar_por").value;
+    const tipo_orden = document.getElementById("select_orden").value;
+
+    if(variable_orden == "id"){
+        if(tipo_orden == "ascendente"){
+            array_filas.sort((a, b) => a.children[1].innerHTML.localeCompare(b.children[1].innerHTML));
+        }else{
+            array_filas.sort((a, b) => b.children[1].innerHTML.localeCompare(a.children[1].innerHTML));
+        }
+    }else if(variable_orden == "modelo"){
+        if(tipo_orden == "ascendente"){
+            array_filas.sort((a, b) => a.children[2].innerHTML.localeCompare(b.children[2].innerHTML));
+        }else{
+            array_filas.sort((a, b) => b.children[2].innerHTML.localeCompare(a.children[2].innerHTML));
+        }
+    }else if(variable_orden == "version"){
+        if(tipo_orden == "ascendente"){
+            array_filas.sort((a, b) => a.children[3].innerHTML.localeCompare(b.children[3].innerHTML));
+        }else{
+            array_filas.sort((a, b) => b.children[3].innerHTML.localeCompare(a.children[3].innerHTML));
+        } 
+    }else if(variable_orden == "color"){
+        if(tipo_orden == "ascendente"){
+            array_filas.sort((a, b) => a.children[4].innerHTML.localeCompare(b.children[4].innerHTML));
+        }else{
+            array_filas.sort((a, b) => b.children[4].innerHTML.localeCompare(a.children[4].innerHTML));
+        }
+    }else if(variable_orden == "precio"){
+        if(tipo_orden == "ascendente"){
+            array_filas.sort((a, b) => a.children[5].innerHTML.localeCompare(b.children[5].innerHTML));
+        }else{
+            array_filas.sort((a, b) => b.children[5].innerHTML.localeCompare(a.children[5].innerHTML));
+        }
+    }else if(variable_orden == "serie"){
+        if(tipo_orden == "ascendente"){
+            array_filas.sort((a, b) => a.children[6].innerHTML.localeCompare(b.children[6].innerHTML));
+        }else{
+            array_filas.sort((a, b) => b.children[6].innerHTML.localeCompare(a.children[6].innerHTML));
+        }
+    }else if(variable_orden == "anio"){
+        if(tipo_orden == "ascendente"){
+            array_filas.sort((a, b) => a.children[7].innerHTML.localeCompare(b.children[7].innerHTML));
+        }else{
+            array_filas.sort((a, b) => b.children[7].innerHTML.localeCompare(a.children[7].innerHTML));
+        }
+    }else if(variable_orden == "proveedor"){
+        if(tipo_orden == "ascendente"){
+            array_filas.sort((a, b) => a.children[8].innerHTML.localeCompare(b.children[8].innerHTML));
+        }else{
+            array_filas.sort((a, b) => b.children[8].innerHTML.localeCompare(a.children[8].innerHTML));
+        }
+    }else if(variable_orden == "estado"){
+        if(tipo_orden == "ascendente"){
+            array_filas.sort((a, b) => a.children[9].innerHTML.localeCompare(b.children[9].innerHTML));
+        }else{
+            array_filas.sort((a, b) => b.children[9].innerHTML.localeCompare(a.children[9].innerHTML));
+        }
+    }else if(variable_orden == "stock"){
+        if(tipo_orden == "ascendente"){
+            array_filas.sort((a, b) => a.children[10].innerHTML.localeCompare(b.children[10].innerHTML));
+        }else{
+            array_filas.sort((a, b) => b.children[10].innerHTML.localeCompare(a.children[10].innerHTML));
+        }
+    }
+
+    for (let i = 0; i < array_filas.length; i++) {
+        tabla.appendChild(array_filas[i]);
+    }
+    //volmemos a
+}
+
+function abrir_vista_auto(parametro_id){
+    alert(parametro_id);
 }
