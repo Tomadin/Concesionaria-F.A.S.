63e6409c6efcb376fa7f7e66193cc8db2ae0cdf3
JoaquinYornet <joaquinyornetderosas@gmail.com>
2025-05-14 15:49:52 -0300

    Nuevo scrollbar y ventana completa para ver vehículo
    Carpeta fonts eliminada, nueva ventana en el stock de los autos, cambios de colores y elementos, nuevo scrollbar personalizado

--- src/main/resources/static/js/script_vista_principal.js
+++ src/main/resources/static/js/script_vista_principal.js
@@ -1,23 +1,25 @@
 function inicio(){ //funcion llamada por el body del html cada vez que se carga la página
 
     array_filas = [];
+    diccionario_atributos = {};
 
     //debug, filas de ejemplo
-    nueva_fila(1, "Logan", "Life 1.6", "Gris", 25000000, "A1234", 2019, "proveedor1", "0Km", 5);
-    nueva_fila(2, "Kangoo", "Zen 1.6", "Rojo", 30000000, "A1634", 2020, "proveedor2", "0Km", 2);
-    nueva_fila(3, "Sandero", "Life 1.6", "Azul", 22000000, "A2345", 2021, "proveedor3", "Usado", 3);
-    nueva_fila(4, "Kardian", "Evolution", "Blanco", 28000000, "A3456", 2022, "proveedor4", "0Km", 4);
-    nueva_fila(5, "Clio", "Mio 1.2", "Negro", 11000000, "H4567", 1994, "proveedor2", "Usado", 1);
-    nueva_fila(6, "Arkana E-Tech Hybrid", "Espirit Alpine", "Gris", 43900000, "A6789", 2024, "proveedor3", "0Km", 1);
-    nueva_fila(7, "Stepway", "Intents 1.6", "Naranja", 28000000, "HS7690", 2025, "proveedor4", "0Km", 1);
-    nueva_fila(8, "Stepway", "Intents 1.6 CVT", "Blanco", 28000000, "HS7691", 2025, "proveedor4", "0Km", 1);
-    nueva_fila(9, "Master", "Furgon L1H1", "Blanco", 54080000, "A6797", 2022, "proveedor5", "0Km", 3);
-    nueva_fila(10, "Alskan", "Confort 2.3 dCi 160 2WD", "Azul", 36360000, "HS4561", 2023, "proveedor2", "0Km", 0);
+    nueva_fila(1, "Logan", "Life 1.6", "Gris", 25000000, "A1234", 2019, "proveedor1", "Disponible", 5, null, null, null, null, null);
+    nueva_fila(2, "Kangoo", "Zen 1.6", "Rojo", 30000000, "A1634", 2020, "proveedor2", "Vendido", 2, null, null, null, null, null);
+    nueva_fila(3, "Sandero", "Life 1.6", "Azul", 22000000, "A2345", 2021, "proveedor3", "Disponible", 3, null, null, null, null, null);
+    nueva_fila(4, "Kardian", "Evolution", "Blanco", 28000000, "A3456", 2022, "proveedor4", "Almacenado", 4, null, null, null, null, null);
+    nueva_fila(5, "Clio", "Mio 1.2", "Negro", 11000000, "H4567", 1994, "proveedor2", "Disponible", 1, null, null, null, null, null, null, null);
+    nueva_fila(6, "Arkana E-Tech Hybrid", "Espirit Alpine", "Gris", 43900000, "A6789", 2024, "proveedor3", "Vendido", 1, null, null, null, null, null);
+    nueva_fila(7, "Stepway", "Intents 1.6", "Naranja", 28000000, "HS7690", 2025, "proveedor4", "Almacenado", 1, null, null, null, null, null);
+    nueva_fila(8, "Stepway", "Intents 1.6 CVT", "Blanco", 28000000, "HS7691", 2025, "proveedor4", "Almacenado", 1, null, null, null, null, null);
+    nueva_fila(9, "Master", "Furgon L1H1", "Blanco", 54080000, "A6797", 2022, "proveedor5", "Disponible", 3, null, null, null, null, null);
+    nueva_fila(10, "Alskan", "Confort 2.3 dCi 160 2WD", "Azul", 36360000, "HS4561", 2023, "proveedor2", "Disponible", 0, null, null, null, null, null);
 }
 
 var array_filas = []; //array que contiene las filas de la tabla
+var diccionario_atributos = {} //diccionario que contiene los atributos cada vehiculo, y de su modelo - serie
 
-function nueva_fila(vehiculo_id, modelo, version, color, precio, serie, anio, proveedor, estado, stock){
+function nueva_fila(vehiculo_id, modelo, version, color, precio, serie, anio, proveedor, estado, stock, imagen_url, motor, carroceria, transmision, puertas){
 
     const tabla = document.getElementById("tabla_stock");
 
@@ -26,7 +28,7 @@ function nueva_fila(vehiculo_id, modelo, version, color, precio, serie, anio, pr
     fila.className = "Fila_stock";
 
     const celda_imagen = document.createElement("td");
-    const imagen = document.createElement("img");
+    const nueva_imagen = document.createElement("img");
 
     const celda_id_vehiculo = document.createElement("td");
     const celda_modelo = document.createElement("td");
@@ -39,9 +41,20 @@ function nueva_fila(vehiculo_id, modelo, version, color, precio, serie, anio, pr
     const celda_estado = document.createElement("td");
     const celda_stock = document.createElement("td");
 
-    imagen.src = "images/imagen-placeholder-para-autos.jpg";
-    imagen.className = "Imagenes_auto_filas";
-    celda_imagen.appendChild(imagen);
+    let imagen_valida = false; 
+    //imagen_valida= checkImage(imagen_url); //IMPORTANTE - NO BORRAR - EN IMPLEMENTACIÓN (es para verificar que la imágen exista, sino se usa una de placeholder)
+
+    if(imagen_url===null || imagen_url===undefined || !imagen_valida){
+        nueva_imagen.src = "images/imagen-placeholder-para-autos.jpg";
+        imagen_url = "images/imagen-placeholder-para-autos.jpg";
+        console.log("Estableciendo imagen por defecto");
+    }else{
+        nueva_imagen.src = imagen_url;
+    }
+    nueva_imagen.src = "images/imagen-placeholder-para-autos.jpg"
+
+    nueva_imagen.className = "Imagenes_auto_filas";
+    celda_imagen.appendChild(nueva_imagen);
 
     celda_id_vehiculo.innerHTML = parseInt(vehiculo_id);
     celda_modelo.innerHTML = modelo;
@@ -68,15 +81,49 @@ function nueva_fila(vehiculo_id, modelo, version, color, precio, serie, anio, pr
 
     // pasa el ID de la fila como parámetro al hacer click en la fila
     fila.addEventListener('click', function() {
-        abrir_vista_auto(this.id); 
+        abrir_ventana_auto(this.id); 
     });
 
     tabla.appendChild(fila);
     //ordenar_tabla();
 
     array_filas.push(fila);
-    
 
+    diccionario_atributos["vehiculo_" + vehiculo_id] = [vehiculo_id, modelo, version, color, precio, serie, anio, proveedor, estado, stock, imagen_url, imagen_valida, motor, carroceria, transmision, puertas];
+}
+
+function checkImage(url) {
+
+    if(url===null || url===undefined){
+        console.log("La URL es null o undefined");
+        return false;
+    }
+
+    var request = new XMLHttpRequest();
+    request.open("GET", url, true);
+    request.send();
+    request.onload = function() {
+        if (request.status === 200) {
+        var urlObj = new URL(url);
+        var extension = urlObj.pathname.split(".").pop().toLowerCase();
+        if (extension === "jpg" || extension === "png" || extension === "gif") {
+            var contentType = request.getResponseHeader("Content-Type");
+            if (contentType.startsWith("image/")) {
+            console.log("La URL es una imagen y existe");
+            return true;
+            } else {
+            console.log("La URL existe, pero no es una imagen");
+            return false;
+            }
+        } else {
+            console.log("La URL existe, pero no tiene una extensión de imagen");
+            return false;
+        }
+        } else {
+        console.log("La URL no existe");
+        return false;
+        }
+    }
 }
 
 function ordenar_tabla(){
@@ -170,10 +217,6 @@ function ordenar_tabla(){
     //volmemos a agregar las filas una vez ordenadas
 }
 
-function abrir_vista_auto(parametro_id){
-    alert(parametro_id);
-}
-
 
 document.getElementById("form_buscar").addEventListener("input", function(e) {
     e.preventDefault();
@@ -181,19 +224,20 @@ document.getElementById("form_buscar").addEventListener("input", function(e) {
     const input_value = document.getElementById("input_buscar").value;
 
 
-    if((!inputBuscar.value || input_value.trim() === "") && document.getElementById("tabla_stock") && document.getElementById("input_buscar")){
-        filtrar_filas();
+    if((!input_value || input_value.trim() === "") && document.getElementById("tabla_stock") && document.getElementById("input_buscar")){
+        filtrar_filas(true);
     }
     
 });
 
-document.getElementById("form_buscar").addEventListener('change', function(e) {
+document.getElementById("form_buscar").addEventListener('submit', function(e) {
     e.preventDefault();
 
-    filtrar_filas();
+    filtrar_filas(false);
 });
 
-function filtrar_filas(){
+
+function filtrar_filas(suprimir_alerta){
     if(document.getElementById("tabla_stock") && document.getElementById("input_buscar")){
 
         const tabla = document.getElementById("tabla_stock");
@@ -212,7 +256,121 @@ function filtrar_filas(){
             }
         });
 
-    }else{
+    }else if(!suprimir_alerta){
         alert("Salir de la página actual");
     }
+}
+
+
+function abrir_ventana_auto(p_id){ //parámetro id
+    console.log(p_id);
+    var ventana = document.getElementById("ventana_auto");
+    ventana.hidden = false;
+    llenar_vista_completa(p_id);
+}
+
+function cerrar_ventana_auto(){
+    var ventana = document.getElementById("ventana_auto");
+    ventana.hidden = true;
+}
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape') {
+    cerrar_ventana_auto();
+  }
+});
+
+function llenar_vista_completa(p_id){
+
+  //[vehiculo_id, modelo, version, color, precio, serie, anio, proveedor, estado, stock, imagen_url, imagen_valida, motor, carroceria, transmision, puertas]
+
+  if(diccionario_atributos[p_id][0]===undefined || diccionario_atributos[p_id][0]===null){
+    document.getElementById("v_c_id").innerHTML = "ID : -";
+  } else {
+    document.getElementById("v_c_id").innerHTML = "ID : " + diccionario_atributos[p_id][0];
+  }
+
+  if(diccionario_atributos[p_id][10]===undefined || diccionario_atributos[p_id][10]===null || diccionario_atributos[p_id][11] === 0){
+    document.getElementById("v_c_imagen").src = "images/imagen-no-disponible.png"; // O una imagen por defecto
+  } else {
+    document.getElementById("v_c_imagen").src = diccionario_atributos[p_id][10];
+  }
+
+  if(diccionario_atributos[p_id][1]===undefined || diccionario_atributos[p_id][1]===null){
+    document.getElementById("v_c_modelo").innerHTML = "Modelo : -";
+  } else {
+    document.getElementById("v_c_modelo").innerHTML = "Modelo : " + diccionario_atributos[p_id][1];
+  }
+
+  if(diccionario_atributos[p_id][2]===undefined || diccionario_atributos[p_id][2]===null){
+    document.getElementById("v_c_version").innerHTML = "Version : -";
+  } else {
+    document.getElementById("v_c_version").innerHTML = "Version : " + diccionario_atributos[p_id][2];
+  }
+
+  if(diccionario_atributos[p_id][4]===undefined || diccionario_atributos[p_id][4]===null){
+    document.getElementById("v_c_precio").innerHTML = "Precio (ARS) : -";
+  } else {
+    document.getElementById("v_c_precio").innerHTML = "Precio (ARS) : $" + diccionario_atributos[p_id][4];
+  }
+
+  if(diccionario_atributos[p_id][7]===undefined || diccionario_atributos[p_id][7]===null){
+    document.getElementById("v_c_proveedor").innerHTML = "Proveedor : -";
+  } else {
+    document.getElementById("v_c_proveedor").innerHTML = "Proveedor : " + diccionario_atributos[p_id][7];
+  }
+
+  if(diccionario_atributos[p_id][3]===undefined || diccionario_atributos[p_id][3]===null){
+    document.getElementById("v_c_color").innerHTML = "Color : -";
+  } else {
+    document.getElementById("v_c_color").innerHTML = "Color : " + diccionario_atributos[p_id][3];
+  }
+
+  if(diccionario_atributos[p_id][5]===undefined || diccionario_atributos[p_id][5]===null){
+    document.getElementById("v_c_serie").innerHTML = "Número de serie : -";
+  } else {
+    document.getElementById("v_c_serie").innerHTML = "Número de serie : " + diccionario_atributos[p_id][5];
+  }
+
+  if(diccionario_atributos[p_id][6]===undefined || diccionario_atributos[p_id][6]===null){
+    document.getElementById("v_c_anio").innerHTML = "Año de fabricación : -";
+  } else {
+    document.getElementById("v_c_anio").innerHTML = "Año de fabricación : " + diccionario_atributos[p_id][6];
+  }
+
+  if(diccionario_atributos[p_id][8]===undefined || diccionario_atributos[p_id][8]===null){
+    document.getElementById("v_c_estado").innerHTML = "Estado del vehículo : -";
+  } else {
+    document.getElementById("v_c_estado").innerHTML = "Estado del vehículo : " + diccionario_atributos[p_id][8];
+  }
+
+  if(diccionario_atributos[p_id][9]===undefined || diccionario_atributos[p_id][9]===null){
+    document.getElementById("v_c_stock").innerHTML = "Stock del modelo-version : -";
+  } else {
+    document.getElementById("v_c_stock").innerHTML = "Stock del modelo-version : " + diccionario_atributos[p_id][9];
+  }
+
+  if(diccionario_atributos[p_id][11]===undefined || diccionario_atributos[p_id][12]===null){
+    document.getElementById("v_c_motor").innerHTML = "Motor : -";
+  } else {
+    document.getElementById("v_c_motor").innerHTML = "Motor : " + diccionario_atributos[p_id][12];
+  }
+
+  if(diccionario_atributos[p_id][12]===undefined || diccionario_atributos[p_id][13]===null){
+    document.getElementById("v_c_carroceria").innerHTML = "Tipo de carroceria : -";
+  } else {
+    document.getElementById("v_c_carroceria").innerHTML = "Tipo de carroceria : " + diccionario_atributos[p_id][13];
+  }
+
+  if(diccionario_atributos[p_id][13]===undefined || diccionario_atributos[p_id][14]===null){
+    document.getElementById("v_c_transmision").innerHTML = "Transmision : -";
+  } else {
+    document.getElementById("v_c_transmision").innerHTML = "Transmision : " + diccionario_atributos[p_id][14];
+  }
+
+  if(diccionario_atributos[p_id][14]===undefined || diccionario_atributos[p_id][15]===null){
+    document.getElementById("v_c_puertas").innerHTML = "Número de puertas : -";
+  } else {
+    document.getElementById("v_c_puertas").innerHTML = "Número de puertas : " + diccionario_atributos[p_id][15];
+  }
 }
